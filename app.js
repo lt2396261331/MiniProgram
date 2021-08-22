5bc30f492502012ad591e722c33fa7e82f74d00f
suyansuyan <yan1283298696@163.com>
2021-08-22 11:22:09 +0800

    注册小程序和页面

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // app.js
+// 注册一个小程序示例
+App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    // 1.判断小程序的进入场景
+    // console.log(options)
+    // 获取用户信息，并且获取到用户的信息之后传给服务器
+    wx.getUserProfile({
+      desc: "获取信息",
+      success: res => {
+        console.log(res)
+      }
+    })
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
+  },
+
+  globalData: {
+    name: 'suyansuyan',
+    age: 18
+  }
+})
